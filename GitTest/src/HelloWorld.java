eff676cbfd95e6dbe856af703529999e464c33b3
DrBennyIng <50904218+DrBennyIng@users.noreply.github.com>
2019-05-22 10:12:48 +0200

    Update HelloWorld.java

--- GitTest/src/HelloWorld.java
+++ GitTest/src/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
-		System.out.println("Hello World 3");
 }
 }
